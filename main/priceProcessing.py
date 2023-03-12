28eff02a4c714840d6e89e2fa7bffc18242d76b8
Pedro Swaby <pswabyjr@gmail.com>
2023-03-12 07:33:32 -0700

    added classes PriceHeap, PriceAverage, Processor, Processor for better modularity

--- main/priceProcessing.py
+++ main/priceProcessing.py
@@ -5,8 +5,95 @@ minimum prices. The prices collected will be stored in a Heap structure.
 '''
 import heapq
 from main.priceCollector import ForexPriceCollector
+from abc import abstractclassmethod
 from main.apiLinks import forexLinks
 from main.logManager import LogManager
+
+class PriceHeap:
+    __priceList = []   # minHeap for storing prices. Used to determine minimum price stored
+    __priceListNegative = [] #maxHeap for storing prices. Used to determine maximum priced stored
+
+    def __init__(self):
+        heapq.heapify(self.__priceList)
+        heapq.heapify(self.__priceListNegative)
+    
+    def getMaximumPrice(self):
+        maxPrice = -1*self.__priceListNegative[0]
+        return maxPrice
+    
+    def getMinimumPrice(self):
+        minPrice = self.__priceList[0]
+        return minPrice
+
+    def addToHeap(self,newValue):
+        heapq.heappush(self.__priceList,newValue)
+        newValue*=-1
+        heapq.heappush(self.__priceListNegative,newValue)
+
+    def getMinHeap(self)->list:
+        return self.__priceList
+
+    def getMaxHeap(self)->list:
+        return self.__priceListNegative    
+    
+    def resetHeap(self):
+        self.__priceList.clear()
+        self.__priceListNegative.clear()
+    
+
+class PriceAverage:
+    __sum = 0.0
+    __num_of_times_added = 0
+
+    def addNewPrice(self, price):
+        self.__sum += price
+        self.__num_of_times_added += 1
+    
+    def getAverage(self)-> int:        
+        return self.__sum/self.__num_of_times_added
+    
+    def getSum(self) -> int:
+        return self.__sum
+    
+    def reset(self):
+        self.__sum = 0.0
+        self.__num_of_times_added = 0
+
+class Processor:
+    @abstractclassmethod
+    def resetProcessor(self): pass
+
+    @abstractclassmethod
+    def processInfo(self, *args): pass
+
+
+class ForexPriceProcessor(Processor):
+    
+    __priceAverage = PriceAverage()
+    __priceHeap = PriceHeap()
+
+    def __init__(self):        
+        self.priceCollector = ForexPriceCollector(forexLinks)
+        self.log = LogManager('log.txt')
+    
+    def resetProcessor(self):
+        self.__priceHeap.resetHeap()
+        self.__priceAverage.reset()
+    
+    def processInfo(self):
+        try:
+            firstPrice,secondPrice = self.priceCollector.getPricing()
+        except Exception as err:
+            self.log.logDebugMessage('PriceProcessor Class, addNewPrice():Failed to retrieve data due to down server')
+            return err
+        else:
+            self.addToList(firstPrice/secondPrice)
+            data  = {}
+            data['sum'] = self.__priceAverage.getSum()
+            data['priceList'] = self.__priceHeap.getMinHeap() #TODO: why do we need this again
+            data['priceListNegative'] = self.__priceHeap.getMaxHeap() #TODO: why do we need this again
+            return data
+
 class PriceProcessor:
     def __init__(self):
         '''
@@ -79,6 +166,7 @@ class PriceProcessor:
     def getAttributes(self):
         return self.sum, self.priceList, self.priceListNegative
 
+    # This violates the SRP principle
     def addNewPrice(self):
         try:
             firstPrice,secondPrice = self.priceCollector.getPricing()

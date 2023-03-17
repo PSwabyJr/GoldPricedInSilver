9b97ec4db9f90428e7c962d17b131670eb399677
Pedro Swaby <pswabyjr@gmail.com>
2023-03-17 11:24:13 -0700

    added more notes for mods

--- main/apiLinks.py
+++ main/apiLinks.py
@@ -1,5 +1,5 @@
 # TODO: Sounds great but I rather put this in a json format that can be changed by anyone. I rather not let anyone go into any python files :)
-
+# This will be deleted since the new JsonManager with FileManager interface can handle this data in Jsonformat
 """
 forexLinks stores the api links from https://forex-data-feed.swissquote.com to collect the price of gold
 and silver in ($USD). Both prices are collected from MT5 server. The server has 3 categories of commodity pricing

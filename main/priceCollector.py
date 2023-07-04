362c208ce24a3ef85afe24704f0cc893aa72ecbd
Pedro Swaby <pswabyjr@gmail.com>
2023-07-04 12:52:16 -0700

    updated pricecollector.py with new classes to closely follow SFP

--- main/priceCollector.py
+++ main/priceCollector.py
@@ -3,101 +3,86 @@
 import time
 import concurrent.futures
 import requests
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 
 class RequestError(Exception):
     pass
 
 class PriceCollector:
     @abstractmethod
-    def getPricing(self): pass
-
-class DataCollector:
+    def getPricing(self): 
+        pass
+    
+class PriceDataFeed(ABC):
+    def __init__(self, apiLinks):
+        self._apiLinks = apiLinks
+        
     @abstractmethod
-    def _getData(self, *args): pass
+    def _parsePriceFromDataFeed(self, response):
+        pass
 
-# TODO: Make this class more modular so it's not hardcoded... what happens if they change their structure or you want to try something else
-class ForexDataFeedSwissquote:
-    def retrieveRequestedPriceFromDataFeed(response):
+    def _getAPIPriceData(self) -> float:
+        response = requests.get(self._apiLinks)    
+        if response.ok:
+            price = self._parsePriceFromDataFeed(response)
+            return price
+        else:
+            raise RequestError(f"Request Unsuccessful. Returned status code {response.status_code} instead of 200!!")
+
+    def getRetrievedPricingFromFeed(self):
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            return executor.map(self._getAPIPriceData(), self._apiLinks)
+
+class ForexDataFeedSwissquote(PriceDataFeed):
+    def _parsePriceFromDataFeed(self, response):
         priceData = response.json()
         MT5ServerPrices = priceData[0]['spreadProfilePrices']
         # Price Type: 'Premium'= 0, 'Prime' = 1, 'Standard' = 2
         priceType = 2
         price = MT5ServerPrices[priceType]['ask']
         return price
-
-class ForexPriceCollector(PriceCollector, DataCollector):
     
-    maximumTimeElapsedAllowedInSeconds = 240.0
+class APITimeout:
 
-    def __init__(self, apiLinks, apiDataFeed):
-        self._apiLinks = apiLinks
-        self._dataFeed = apiDataFeed
+    def __init__(self, maxAllowedElapsedTimeInSeconds = 240.0):
+        self._maxAllowedElapsedTimeInSeconds = maxAllowedElapsedTimeInSeconds
         self._beginningTimeInSeconds = 0.0
-
-    def _getparsedPricingFromDataFeed(self,response):
-        price = self._dataFeed.retrieveRequestedPriceFromDataFeed(response)
-        return price
-        
-    def _getData(self) -> float:
-        response = requests.get(self._apiLinks)    
-        if response.ok:
-            price = self._getparsedPricingFromDataFeed(response)
-            return price
-        else:
-            raise RequestError(f"Request Unsuccessful. Returned status code {response.status_code} instead of 200!!")
-
-    def _getNewTimeElapsedInSeconds(self):
-        currentTime = time.time()
-        timeElapsedInSeconds = currentTime - self._beginningTimeInSeconds
-        return timeElapsedInSeconds
     
-    def _getRetrievedPricingFromFeed(self):
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            return executor.map(self._getData, self._apiLinks)
-        
-    def _isTimeUpAfterMultipleAPICalls(self):
-        timeElapsedInSeconds = self._getNewTimeElapsedInSeconds()
-        if timeElapsedInSeconds >= ForexPriceCollector.maximumTimeElapsedAllowedInSeconds:
+    def setNewBeginningTimeInSeconds(self, newTimeInSeconds):
+        self._beginningTimeInSeconds = newTimeInSeconds
+
+    def _isTimeElapsedExceedMaximumAllowed(self):
+        currentTimeInSeconds = time.time()
+        timeElapsedInSeconds = currentTimeInSeconds - self._beginningTimeInSeconds
+        if timeElapsedInSeconds >= self._maxAllowedElapsedTimeInSeconds:
             return True
         else:
             return False
 
-    def getPricing(self) -> list:
-        IsStillWaitingForPricing = True
-        priceResults = []
-        self._beginningTimeInSeconds = time.time()
+    def isTimeUpAfterFailedAttempts(self):
+        return self._isTimeElapsedExceedMaximumAllowed()  
+    
+class ForexPriceCollector(PriceCollector):
+    
+    def __init__(self, apiDataFeed: PriceDataFeed):
+        self._dataFeed = apiDataFeed
+        self._timeout = APITimeout()
         
-        while IsStillWaitingForPricing:
+    def getPricing(self) -> list:
+        stillWaitingForPricing = True
+        priceResults = []
+        self._timeout.setNewBeginningTimeInSeconds(time.time())
+        
+        while stillWaitingForPricing:
             try:
-                for result in self._getRetrievedPricingFromFeed():
+                fetchedData = self._dataFeed.getRetrievedPricingFromFeed()
+                for result in fetchedData:
                     priceResults.append(result)
             except RequestError as err:
                 priceResults.clear()
-                if self._isTimeUpAfterMultipleAPICalls():
-                    IsStillWaitingForPricing = False
+                if self._timeout.isTimeUpAfterFailedAttempts():
+                    stillWaitingForPricing = False
                 continue
             else:
-                IsStillWaitingForPricing = False
+                stillWaitingForPricing = False
         return priceResults
-    
-    # TODO: Removed the below lines once getPricing() gets updated
-    # def getPricing(self) -> list:
-    #     IsStillWaitingForPricing = True
-    #     priceResults = []
-    #     beginningTimeInSeconds = time.time()
-    #     timeElapsedInSeconds = 0.0
-    #     while IsStillWaitingForPricing and timeElapsedInSeconds < 240.0:
-    #         with concurrent.futures.ThreadPoolExecutor() as executor:
-    #             try:
-    #                 for result in self._getRetrievedPricingFromFeed():
-    #                     priceResults.append(result)
-    #             except Exception as err:
-    #                 timeElapsedInSeconds = self._getNewTimeElapsedInSeconds(beginningTimeInSeconds)
-    #                 priceResults.clear()
-    #                 if timeElapsedInSeconds >= 240.0:
-    #                     pass
-    #                 continue
-    #             else:
-    #                 IsStillWaitingForPricing = False
-    #     return priceResults

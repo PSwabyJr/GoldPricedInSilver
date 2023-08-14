75379e212cfe2962b16e4cd3aec8412d11ab3687
Pedro Swaby <pswabyjr@gmail.com>
2023-08-14 04:48:01 -0700

    changed variable currentTime to currentDateTime

--- main/marketStatus.py
+++ main/marketStatus.py
@@ -7,15 +7,15 @@ class ForexMarketStatus:
  
     @staticmethod
     def isMarketOpened()->bool:
-        currentTime = datetime.now(EST_TIMEZONE)
-        todayDate = currentTime.weekday()
+        currentDateTime = datetime.now(EST_TIMEZONE)
+        todayDate = currentDateTime.weekday()
 
         # Forex market trades between Sunday 5 pm EST and Friday 4 pm EST
-        if DaysOfWeekMonitor.isTodaySunday(todayDate) and DaysOfWeekMonitor.isCurrentTimePast5pm(currentTime):
+        if DaysOfWeekMonitor.isTodaySunday(todayDate) and DaysOfWeekMonitor.isCurrentTimePast5pm(currentDateTime):
             return True
         elif DaysOfWeekMonitor.isTodayBetweenMondayandThursday(todayDate):
             return True
-        elif DaysOfWeekMonitor.isTodayFriday(todayDate) and DaysOfWeekMonitor.isCurrentTimeNotPast4pm(currentTime):
+        elif DaysOfWeekMonitor.isTodayFriday(todayDate) and DaysOfWeekMonitor.isCurrentTimeNotPast4pm(currentDateTime):
             return True
         else:
             return False
